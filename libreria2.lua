3705c23327cb896e1b209c2eb26b8a59a8f104e7
Luisito <118145449+Atlasv1@users.noreply.github.com>
2023-08-10 01:49:51 -0500

    Update libreria2.lua

--- libreria2.lua
+++ libreria2.lua
@@ -72,7 +72,7 @@ Background.Size = UDim2.new(0, 567, 0, 310)
 local toggledui = false
 		UserInputService.InputBegan:Connect(function(input)
 			pcall(function()
-				if input.KeyCode == Enum.KeyCode.Control then
+				if input.KeyCode == Enum.KeyCode.TabControl then
 					if toggledui == false then
 						toggledui = true
 						UICopied.Enabled = false
